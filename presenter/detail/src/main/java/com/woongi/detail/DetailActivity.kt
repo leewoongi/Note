20878411f49e9fd69a5b91cfa561a205f7874dfa
mynamewoon <mynamewoon@gmail.com>
2025-04-14 00:48:02 +0900

    popupmenu 생성

--- presenter/detail/src/main/java/com/woongi/detail/DetailActivity.kt
+++ presenter/detail/src/main/java/com/woongi/detail/DetailActivity.kt
@@ -1,8 +1,14 @@
 package com.woongi.detail
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.ImageView
+import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.activity.viewModels
+import androidx.annotation.NonNull
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.lifecycle.lifecycleScope
@@ -12,9 +18,11 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.woongi.detail.recyclerview.DetailRecyclerViewAdapter
 import com.woongi.detail.recyclerview.event.createSwipeCallback
+import com.woongi.navigator.api.Destination
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.launch
+
 
 @AndroidEntryPoint
 class DetailActivity : AppCompatActivity() {
@@ -25,6 +33,7 @@ class DetailActivity : AppCompatActivity() {
     private lateinit var recyclerView: RecyclerView
     private lateinit var recyclerViewAdapter: DetailRecyclerViewAdapter
     private lateinit var subTitleTextView: TextView
+    private lateinit var settingButton: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -54,7 +63,7 @@ class DetailActivity : AppCompatActivity() {
         faButton = findViewById(R.id.fab_add)
         faButton.setOnClickListener { viewModel.navigateHome() }
         subTitleTextView = findViewById(R.id.tv_sub_title)
-
+        settingButton = findViewById(R.id.btn_more)
 
         recyclerView = findViewById(R.id.rv_detail)
         recyclerViewAdapter = DetailRecyclerViewAdapter(
@@ -69,8 +78,23 @@ class DetailActivity : AppCompatActivity() {
             adapter = recyclerViewAdapter
             layoutManager = LinearLayoutManager(context)
             createSwipeCallback(
-                onSwipe = { position -> recyclerViewAdapter.remove(position = position) }
+                onSwipe = { position ->
+                    recyclerViewAdapter.remove(position = position) }
             ).attachToRecyclerView(this)
+        }
+
+        settingButton.setOnClickListener {
+            val popupMenu = PopupMenu(this@DetailActivity, settingButton)
+            menuInflater.inflate(R.menu.popupmenu, popupMenu.menu)
+            popupMenu.setOnMenuItemClickListener { item ->
+                when (item.itemId) {
+                    R.id.item_image -> {
+                      
+                    }
+                }
+                false
+            }
+            popupMenu.show()
         }
     }
 }
